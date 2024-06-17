254af7595a75d94b59923e1f93c8ccf06b9b9fc7
Aleksandr <sh17aleksandr@gmail.com>
2024-06-17 15:59:47 +0300

    Initial commit

---
+++ laravel10/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Main\IndexController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
+});
+
+// --- Если без namespace в laravel10/app/Providers/RouteServiceProvider.php
+// Route::group(['namespace' => 'Main'], function () {
+//     Route::get('/', [IndexController::class, '__invoke']);
+// });
+// --------------------------------------------------------------------------
+Auth::routes();
+
+
